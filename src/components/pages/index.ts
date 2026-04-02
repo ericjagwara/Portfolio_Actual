7eca101431173d5c767042df3cca0e5794c42ba8
Eric Jagwara <145301427+ericjagwara@users.noreply.github.com>
2026-04-02 04:25:54 +0300

    update

--- src/components/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export { HomePage } from "./HomePage";
-export { ExperiencePage } from "./ExperiencePage";
-export { ResearchPage } from "./ResearchPage";
-export { CommunityPage } from "./CommunityPage";
-export { OpenSourcePage } from "./OpenSourcePage";
-export { BlogPage } from "./BlogPage";
-export { EventsPage } from "./EventsPage";
